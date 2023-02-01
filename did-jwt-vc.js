d20ba82d2ea55697af3ccfe2405bbff784c575ac
Peter <pfoytik@odu.edu>
2023-01-31 19:08:38 -0500

    vc test

--- did-jwt-vc.js
+++ did-jwt-vc.js
@@ -1,31 +1,31 @@
 // import { JwtCredentialPayload, createVerifiableCredentialJwt } from 'did-jwt-vc'
-import { ES256KSigner, hexToBytes } from 'did-jwt';
+import { ES256KSigner, hexToBytes, decodeJWT } from 'did-jwt';
 import { createVerifiableCredentialJwt, createVerifiablePresentationJwt, verifyCredential, verifyPresentation } from 'did-jwt-vc'
 import { Resolver } from 'did-resolver'
 import { getResolver } from 'web-did-resolver'
 
 
 // Create a singer by using a private key (hex).
-const key = '8eb63d435de4d634bc5f3df79c361e9233f55c9c2fca097758eefb018c4c61df';
+const key = '79fe6ebcdc25e141b17078454f5dedaee1859d3d8b6d0530067d697b5819c810';
 const signer = ES256KSigner(hexToBytes(key))
 
 // Prepare an issuer
 const issuer = {
-    did: 'did:web:skounis.github.io',
+    did: 'did:web:pfoytik.github.io',
     signer: signer
 }
 
 // Prepare the Verifiable Credential Payload
 const vcPayload = {
-  sub: 'did:web:skounis.github.io',
+  sub: 'did:web:pfoytik.github.io',
   nbf: 1562950282,
   vc: {
     '@context': ['https://www.w3.org/2018/credentials/v1'],
     type: ['VerifiableCredential'],
     credentialSubject: {
       degree: {
-        type: 'BachelorDegree',
-        name: 'Baccalauréat en musiques numériques'
+        type: 'MastersDegree',
+        name: 'Masters in Modeling and Simulation'
       }
     }
   }
@@ -55,6 +55,8 @@ console.log('\n//// Verifiable Presentation:\n', vpJwt)
 const resolver = new Resolver(getResolver())
 
 // Verify the Credentantial and the Presentation
+const decodedCred = decodeJWT(vcJwt)
+console.log('///// Degree Type:\n', decodedCred['payload']['vc']['credentialSubject']['degree'])
 const verifiedVC = await verifyCredential(vcJwt, resolver)
 console.log('//// Verified Credentials:\n', verifiedVC)
 
